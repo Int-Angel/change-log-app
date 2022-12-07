fa0a14fd308e5568f17cbfbaf9122ec4eef35603
Int-Angel <Padilla.angel88@gmail.com>
2022-12-06 21:18:17 -0600

    Delete points action

--- src/components/PointsList.jsx
+++ src/components/PointsList.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { CgClose } from "react-icons/cg";
+import axios from "axios";
 
 function PointsList({ points }) {
-  const listItems = points.map((point) => (
+  const [list, setList] = useState(points);
+
+  const handleDeletePoint = (point_id) => {
+    const deletePointURL = "http://localhost:8080/point/delete";
+    const token = localStorage.getItem("token");
+    axios.delete(deletePointURL, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        point_id: point_id,
+      },
+    });
+
+    const newList = list.filter((point) => point.point_id !== point_id);
+    setList(newList);
+  };
+
+  const listItems = list.map((point) => (
     <li className="point" key={point.point_id}>
       <div className="pointContainer">
         <div className="pointDescription">{point.description}</div>
-        <CgClose className="deletePoint" />
+        <CgClose
+          className="deletePoint"
+          onClick={() => handleDeletePoint(point.point_id)}
+        />
       </div>
     </li>
   ));
