c58c483679d0afe45ab0f1b11bab3cc2ec16fd02
Int-Angel <Padilla.angel88@gmail.com>
2022-12-06 21:28:17 -0600

    delete updates

--- src/components/UpdatesList.jsx
+++ src/components/UpdatesList.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import PointsList from "./PointsList";
 import { CgAdd } from "react-icons/cg";
 import { CgClose } from "react-icons/cg";
+import axios from "axios";
 
 function UpdatesList({ updates, openCreatePoint }) {
-  const listItems = updates.map((update) => (
+  const [list, setList] = useState(updates);
+
+  const handleDeleteUpdate = (project_update_id) => {
+    const deleteUpdateURL = "http://localhost:8080/update/delete";
+    const token = localStorage.getItem("token");
+    axios.delete(deleteUpdateURL, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        project_update_id: project_update_id,
+      },
+    });
+
+    const newList = list.filter(
+      (update) => update.project_update_id !== project_update_id
+    );
+    setList(newList);
+  };
+
+  const listItems = list.map((update) => (
     <li className="update" key={update.project_update_id}>
       <div className="updateDescriptionContainer">
         <div className="updateDescription">{update.description}</div>
-        <CgClose className="deleteUpdate" />
+        <CgClose
+          className="deleteUpdate"
+          onClick={() => handleDeleteUpdate(update.project_update_id)}
+        />
       </div>
       <PointsList points={update.points} />
       <CgAdd
